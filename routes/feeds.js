59dfee5b97d0cd6ebfb243652e6e849ae7d90709
matt zhou <matt@cruelyouth.com>
2021-08-09 19:29:21 +0800

    feed url update

--- routes/feeds.js
+++ routes/feeds.js
@@ -7,23 +7,23 @@ const writeFeeds = () => writeApiJSON('./feeds.json', feedsData)
 const feedsData = readFeeds()
 const feeds = feedsData.data
 
-router.get('/:page?', (req, res) => {
-  const { page = 1 } = req.params
-  const pageSize = 10
-  const ret = {
-    feeds: feeds.slice(page * pageSize, (page + 1) * pageSize),
-  }
-  res.json(ret)
-})
-
 router.get('/count', (req, res) => {
   const ret = {
-    count: feeds.length,
+    data: {count: feeds.length}
   }
   res.json(ret)
 })
 
-router.get('/:id/hot', (req, res) => {
+router.get('/', (req, res) => {
+  const { page = 1 } = req.query
+  const pageSize = 10
+  const ret = {
+    data: {feeds: feeds.slice(page * pageSize, (page + 1) * pageSize),}
+  }
+  res.json(ret)
+})
+
+router.update('/:id/hot', (req, res) => {
   const id = parseInt(req.params.id)
   const feed = feeds.find((f) => f.id === id)
   if (feed) {
